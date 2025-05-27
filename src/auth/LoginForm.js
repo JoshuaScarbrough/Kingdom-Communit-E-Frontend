86c916a019c2b69c28e6ce3faca6999064a60d8b
Joshua Scarbrough <joshscar1418@gmail.com>
2025-05-26 22:43:41 -0500

    Trying to deploy

--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import "./LoginForm.css";
-import BASE_URL from '../config';
+import BASE_URL from '../config.js';
 
 
 function LoginForm() {
